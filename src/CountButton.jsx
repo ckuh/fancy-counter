d03b34913eee19157ee2272672e7d99304ee5961
ckuh <kuhchris@gmail.com>
2024-10-19 16:34:52 -0700

    fix: pass disabled down properly to CountButton

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,14 +1,19 @@
 import PropTypes from "prop-types";
 
-export default function CountButton({ setCount, Icon }) {
+export default function CountButton({ disabled = false, setCount, Icon }) {
   return (
-    <button className="count-btn" onClick={() => setCount()}>
+    <button
+      disabled={disabled}
+      className="count-btn"
+      onClick={() => setCount()}
+    >
       <Icon className="count-btn-icon" />
     </button>
   );
 }
 
 CountButton.propTypes = {
+  disabled: PropTypes.bool,
   setCount: PropTypes.func,
   Icon: PropTypes.elementType,
 };
